c19557996ccddb7c0dcb69b4cd95a4aa87c1909b
xeviegido <Xidostudio@gmail.com>
2023-02-10 10:27:58 +0100

    hola

--- Editor/AnimacioPerCodi_Boto_Inspector.cs
+++ Editor/AnimacioPerCodi_Boto_Inspector.cs
@@ -10,15 +10,24 @@ public class AnimacioPerCodi_Boto_Inspector : Editor
     bool mostrar2 = false;
     bool mostrar3 = false;
     bool mostrar4 = false;
+
+    AnimacioPerCodi_Boto _target;
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        AnimacioPerCodi_Boto _target = (AnimacioPerCodi_Boto)target;
+        _target = (AnimacioPerCodi_Boto)target;
         Animacio_Inspector_Addings.MostrarOpcions("ADD onClick", _target, _target.OnClick.Animacions, ref mostrar1);
         Animacio_Inspector_Addings.MostrarOpcions("ADD onEnter", _target, _target.OnEnter.Animacions, ref mostrar2);
         Animacio_Inspector_Addings.MostrarOpcions("ADD onExit", _target, _target.OnExit.Animacions, ref mostrar3);
         Animacio_Inspector_Addings.MostrarOpcions("ADD Loop", _target, _target.Loop.Animacions, ref mostrar4);
 
+        
+    }
+
+
+    private void OnDisable()
+    {
         AssetDatabase.SaveAssetIfDirty(_target);
     }
 }
