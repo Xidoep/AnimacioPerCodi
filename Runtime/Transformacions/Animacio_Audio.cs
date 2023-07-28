19fb12cd50f27c34da21009263039b6241f4ebbb
xeviegido <Xidostudio@gmail.com>
2023-07-28 17:53:29 +0200

    audio falta provar

--- Runtime/Transformacions/Animacio_Audio.cs
+++ Runtime/Transformacions/Animacio_Audio.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XS_Utils;
 
 [System.Serializable]
 public class Animacio_Audio : Animacio
@@ -17,6 +18,7 @@ public class Animacio_Audio : Animacio
 
     [Tooltip("It just have a range to limit it's amount. It's not related to the animation time")]
     [SerializeField] [Range(0,1.5f)] float delay = 0;
+    [SerializeField, Range(0, 1)] float volum = 1;
 
 
 
@@ -25,11 +27,11 @@ public class Animacio_Audio : Animacio
         if (frame > 0)
             return;
 
-        ((AudioSource)objectiu).clip = audio;
+        AudioSource.PlayClipAtPoint(audio, objectiu.transform.position, volum);
 
-        if (delay < 0) ((AudioSource)objectiu).Play();
-        else ((AudioSource)objectiu).PlayDelayed(delay);
+        if (delay < 0) AudioSource.PlayClipAtPoint(audio, objectiu.transform.position, volum);
+        else XS_Coroutine.StartCoroutine_Ending_FrameDependant<Component>(delay, Play, objectiu);
     }
 
-
+    void Play(Component objectiu) => AudioSource.PlayClipAtPoint(audio, objectiu.transform.position, volum);
 }
